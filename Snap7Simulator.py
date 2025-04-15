8be37bd0ea26238e72b86fa42d84ece8da9b6ac4
Sebastian Jennen <sj@imagearts.de>
2025-04-15 17:04:15 +0200

    snap7 possible fix default db

--- Snap7Simulator.py
+++ Snap7Simulator.py
@@ -17,8 +17,9 @@ class Snap7Simulator(Device, metaclass=DeviceMeta):
         self.get_device_properties(self.get_device_class())
         self.info_stream("Connecting to port: " + str(self.port))
         self.server = Server()
+        self.server.start(tcp_port=self.port)
+        # self.server.start(tcp_port=self.port, init_standard_values=True)
         # self.server.register_area(SrvArea.DB,1,bytearray(1024))
-        self.server.start(tcp_port=self.port, init_standard_values=True)
         self.set_state(DevState.ON)
 
 if __name__ == "__main__":
