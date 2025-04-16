5a0e64210720b4731dce0cf86a68e4a850e3fca4
Sebastian Jennen <sj@imagearts.de>
2025-04-16 13:09:14 +0200

    snap7: possible thread fix for simulator server

--- Snap7Simulator.py
+++ Snap7Simulator.py
@@ -1,3 +1,5 @@
+# see also: https://python-snap7.readthedocs.io/en/latest/_modules/snap7/server.html
+
 import time
 from tango import AttrQuality, AttrWriteType, DispLevel, DevState, Attr, CmdArgType, UserDefaultAttrProp
 from tango.server import Device, attribute, command, DeviceMeta
@@ -7,19 +9,21 @@ import os
 import datetime
 import snap7
 from snap7.server import Server
+from snap7.server import mainloop
+from threading import Thread
 
 class Snap7Simulator(Device, metaclass=DeviceMeta):
     pass
     port = device_property(dtype=int, default_value=10002)
 
+    def runServer(self):
+        mainloop(tcp_port=self.port, init_standard_values=False)
+
     def init_device(self):
         self.set_state(DevState.INIT)
         self.get_device_properties(self.get_device_class())
         self.info_stream("Connecting to port: " + str(self.port))
-        self.server = Server()
-        self.server.start(tcp_port=self.port)
-        # self.server.start(tcp_port=self.port, init_standard_values=True)
-        # self.server.register_area(SrvArea.DB,1,bytearray(1024))
+        Thread(target=self.runServer).start()
         self.set_state(DevState.ON)
 
 if __name__ == "__main__":
